e0748451443218723370e7aad290c6c2e5f28bb7
unknown <jsiew@cc593.iselect1.com.au>
2015-03-06 15:50:29 +1100

    Using regular_season_detailed_results data set

--- NCAA/analyseData.R
+++ NCAA/analyseData.R
@@ -4,7 +4,8 @@
 library(RODBC)
 conn <- odbcConnect("EVORA")
 # query <- "select * from kaggle.dbo.vw_regular_season_team_stats"
-query <- "select * from kaggle.dbo.vw_regular_season_detailed_results_doubled"
+# query <- "select * from kaggle.dbo.vw_regular_season_detailed_results_doubled"
+query <- "select * from kaggle.dbo.regular_season_detailed_results"
 data <- sqlQuery(conn, query)
 data$id <- seq(1,nrow(data))
 
